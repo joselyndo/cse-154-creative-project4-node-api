2fa58d180dda7f42d23acdee945b59d90777ac53
Joselyn Do <joselynd@uw.edu>
2024-05-15 22:56:43 -0700

    OK HTML and CSS

--- public/index.js
+++ public/index.js
@@ -13,3 +13,16 @@
   }
 
 })();
+
+{/* <img src="../img/zinnia.jpeg" alt="aster">
+<section>
+  <h3>Aster</h3>
+  <p>
+    Sunflowers are originally from North America and have been cultivated
+    for over 4,500 years. This is partly because the entirety of the
+    sunflower plant is edible, so the leaves, stalks, and roots could all
+    be used as food. In fact, sunflowers were grown as food in North America
+    before other crops such as corn became commonplace.
+  </p>
+  <p><a href="">Image link</a> | <a href="">Fun fact link</a></p>
+</section> */}
