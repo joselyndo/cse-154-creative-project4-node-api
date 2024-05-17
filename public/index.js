c5636d3c56916edf160eab1263e5a84397c47570
Joselyn Do <joselynd@uw.edu>
2024-05-17 00:28:35 -0700

    Add default flower path and add starting functions for front end

--- public/index.js
+++ public/index.js
@@ -5,16 +5,73 @@
 "use strict";
 
 (function() {
+  const START_VIEW = "start-view";
+  const PATH_VIEW = "path-selection-view";
+  const FLOWER_VIEW = "flower-view";
+  const END_VIEW = "end-view";
 
-  windows.addEventListener("load", init);
+  window.addEventListener("load", init);
 
   function init() {
+    document.getElementById("start-btn").addEventListener("click", function() {
+      nextView(START_VIEW, PATH_VIEW);
+    });
+    initializePathView();
+  }
+
+  function nextView(currView, nextView) {
+    document.getElementById(currView).classList.toggle("hidden");
+    document.getElementById(currView).classList.toggle("visible");
+
+    document.getElementById(nextView).classList.toggle("hidden");
+    document.getElementById(nextView).classList.toggle("visible");
+  }
+
+  function initializePathView() {
+    let inputArr = document.querySelectorAll("input");
+    for (let input = 0; input < inputArr.length; input++) {
+      inputArr[input].addEventListener("change", updatePathView);
+    }
+
+    let pathOptionBtns = document.querySelectorAll("#paths button");
+    for (let button = 0; button < pathOptionBtns.length; button++) {
+      pathOptionBtns[button].addEventListener("click", function() {
+        nextView(PATH_VIEW, FLOWER_VIEW);
+        console.log("u");
+        initializeFlowersView();
+      });
+    }
+  }
+
+  // update path flowers according to num flowers chosen
+  function updatePathView() {
+  }
+
+  // adds the first flower to screen
+  function initializeFlowersView() {
+
+  }
+
+  // Goes to either the next flower or to the end screen
+  function nextScreenFromFlowerView() {
+
+  }
+
+  function initializeEndView() {
+
+  }
+
+  function addRecommendation() {
 
   }
 
 })();
 
-{/* <img src="../img/zinnia.jpeg" alt="aster">
+// API: get list of random flowers of length 5 -> change
+// API: get info for a specific flower -> dblclick l/r buttons
+// API: send if recommend or not -> click
+
+/* <img src="../img/zinnia.jpeg" alt="aster">
 <section>
   <h3>Aster</h3>
   <p>
@@ -25,4 +82,4 @@
     before other crops such as corn became commonplace.
   </p>
   <p><a href="">Image link</a> | <a href="">Fun fact link</a></p>
-</section> */}
+</section> */
